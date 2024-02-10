cfd8c4832ceb3c20e5a0abc44fd3a579a67a2d52
Ifham-Hashir <ifham09052003@gmail.com>
2024-02-10 08:06:44 +0530

    Add files via upload

---
+++ Quiz-Application-Using-Java-master/src/quiz/application/Rules.java
@@ -0,0 +1,72 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to QUIZIFY");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1. Absolutely no copying, using unauthorized materials." + "<br><br>" +
+                "2. Keep all electronic devices away unless specifically permitted." + "<br><br>" +
+                "3. Adhere to all provided guidelines regarding time limits." + "<br><br>" +
+                "4. Stop working on a question once the allocated time is up." + "<br><br>" +
+                "5. Provide answers to the best of your knowledge and avoid guessing." + "<br><br>" +
+                "6. Avoid plagiarism or unauthorized collaboration." + "<br><br>" +
+                "7. Submit work within the specified timeframe." + "<br><br>" +
+                "8. Maintain a quiet environment; no talking during the exam." + "<br><br>" +
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
